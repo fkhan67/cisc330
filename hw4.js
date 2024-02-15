a42d6d12223140ffb70d72aada4968f40c86d8bc
fkhan67 <157556820+fkhan67@users.noreply.github.com>
2024-02-14 19:28:21 -0500

    Create hw4.js

---
+++ hw4.js
@@ -0,0 +1,70 @@
+const cats = [
+    {
+        name: 'Charlie',
+        adoptionStatus: 'available'
+    },
+    {
+        name: 'Lily',
+        adoptionStatus: 'not-available'
+    },
+    {
+        name: 'Coco',
+        adoptionStatus: 'available'
+    },
+    {
+        name: 'Oreo',
+        adoptionStatus: 'not-available'
+    },
+    {
+        name: 'Luna',
+        adoptionStatus: 'available'
+    },
+    {
+        name: 'Milo',
+        adoptionStatus: 'available'
+    },
+    {
+        name: 'Lola',
+        adoptionStatus: 'not-available'
+    },
+    {
+        name: 'Leo',
+        adoptionStatus: 'available'
+    },
+    {
+        name: 'Willow',
+        adoptionStatus: 'available'
+    },
+    {
+        name: 'Bella',
+        adoptionStatus: 'not-available'
+    },
+    {
+        name: 'Max',
+        adoptionStatus: 'available'
+    },
+    {
+        name: 'Cleo',
+        adoptionStatus: 'available'
+    },
+    {
+        name: 'Lucy',
+        adoptionStatus: 'not-available'
+    },
+    {
+        name: 'Daisy',
+        adoptionStatus: 'available'
+    },
+];
+
+const result = [];
+
+for (const cat of cats) {
+    if (cat.adoptionStatus === 'available') {
+        result.push(cat.name);
+    }
+}
+
+const sentence = `Congratulations! You have adopted ${result.length} cats: ${result.join(', ')}.`;
+
+console.log(sentence);
